d474d36b6e526197d46f765a7879cbc4e79ee402
Sutharsan Pushparajah <sutharsan.pushparajah@capgemini.com>
2021-02-11 17:49:58 +0100

    cleaning

--- index.js
+++ index.js
@@ -4,15 +4,21 @@ const privateresources = require('./privateresources')
 
 if (process.env.SERVER_ENV !== 'production') {
     require('dotenv').config()
-}
+    const http = require('http')
+    const https = require('https')
+    const fs = require('fs')
 
-const http = require('http')
-const https = require('https')
-const fs = require('fs')
+    const options = {
+        key: fs.readFileSync('selfsigned.key'),
+        cert: fs.readFileSync('selfsigned.crt')
+    }
 
-const options = {
-    key: fs.readFileSync('selfsigned.key'),
-    cert: fs.readFileSync('selfsigned.crt')
+    http.createServer(app).listen(8080);
+    https.createServer(options, app).listen(8443);
+
+    app.listen(8000, () => {
+        console.log('Server is running')
+    })
 }
 
 const app = express()
@@ -22,11 +28,3 @@ app.use('',privateresources)
 app.get('/', function (req, res) {
     res.send('Welcome to Node.JS Auth with Salesforce');
 });
-
-http.createServer(app).listen(8080);
-https.createServer(options, app).listen(8443);
-
-app.listen(8000, () => {
-    console.log('Server is running')
-})
-
