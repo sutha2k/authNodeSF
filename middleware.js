8981ad15045f7c4ef9a8892af21837aa8219b28b
Sutharsan Pushparajah <sutharsan.pushparajah@capgemini.com>
2021-02-12 11:58:02 +0100

    cleaning

--- middleware.js
+++ middleware.js
@@ -1,13 +1,13 @@
 const jsforce = require('jsforce')
 
+process.on('uncaughtException', err => {
+  console.error('There was an uncaught error', err)
+  process.exit(1) //mandatory (as per the Node.js docs)
+})
+
 module.exports = function(req, res, next) {
   const token = req.headers["bearer"];
   const instanceUrl = req.headers["instanceurl"];
-
-  process.on('uncaughtException', err => {
-    console.error('There was an uncaught error', err)
-    res.status(400).send("Invalid token.");
-  })
 
   if (!token || !instanceUrl) return res.status(401).send("Access denied. No token provided.");
 
