055f836194431e8f68cecb45a66896995b75f60c
Sutharsan Pushparajah <sutharsan.pushparajah@capgemini.com>
2021-03-08 18:40:41 +0100

    debug

--- middleware.js
+++ middleware.js
@@ -1,8 +1,9 @@
 const jsforce = require('jsforce')
 
 process.on('uncaughtException', err => {
-  console.error('There was an uncaught error', err)
-  process.exit(1) //mandatory (as per the Node.js docs)
+  console.error('There was an uncaught error', err);
+  res.status(400).send("Invalid token.");
+  process.exit(1); //mandatory (as per the Node.js docs)
 })
 
 module.exports = function(req, res, next) {
