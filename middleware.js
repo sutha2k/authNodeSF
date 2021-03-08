692e98592f3b745ea99d9f0f2141fe9ae5460649
Sutharsan Pushparajah <sutharsan.pushparajah@capgemini.com>
2021-03-08 18:58:02 +0100

    debug

--- middleware.js
+++ middleware.js
@@ -11,6 +11,8 @@ module.exports = function(req, res, next) {
 
   if (!token || !instanceUrl) return res.status(401).send("Access denied. No token provided.");
 
+  console.log("bearer ", token);
+  console.log("instanceUrl ", instanceUrl);
   try {
     const conn = new jsforce.Connection({
         instanceUrl : instanceUrl,
