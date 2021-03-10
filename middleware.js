b472db5fe471283b89efd4df72ebfb9d31709ea5
Sutharsan Pushparajah <sutharsan.pushparajah@capgemini.com>
2021-03-10 17:19:31 +0100

    debug

--- middleware.js
+++ middleware.js
@@ -9,6 +9,8 @@ module.exports = function(req, res, next) {
   const token = req.headers["bearer"];
   const instanceUrl = req.headers["instanceurl"];
 
+  console.log(JSON.stringify(req.headers));
+
   if (!token || !instanceUrl) return res.status(401).send("Access denied. No token provided.");
 
   console.log("bearer ", token);
