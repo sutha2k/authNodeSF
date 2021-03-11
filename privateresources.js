b80206473d72e243ac26c8280929c032055dfd18
Sutharsan Pushparajah <sutharsan.pushparajah@capgemini.com>
2021-03-11 12:21:23 +0100

    catch err

--- privateresources.js
+++ privateresources.js
@@ -2,7 +2,10 @@ const middleware = require("./middleware");
 const express = require("express");
 const router = express.Router();
 
-router.get("/secured", middleware, async (req, res) => {
+router.get("/secured", middleware, async (err, req, res, next) => {
+    if (err){
+        return res.status(401).send("Access denied");
+    }
     res.send('Accessing secured resource');
 });
 
