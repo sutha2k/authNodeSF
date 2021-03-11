67f66fbc9bfe688e859de30b1be3a4cc60da7c0a
Sutharsan Pushparajah <sutharsan.pushparajah@capgemini.com>
2021-03-11 13:28:17 +0100

    cleaning

--- privateresources.js
+++ privateresources.js
@@ -3,7 +3,7 @@ const express = require("express");
 const router = express.Router();
 
 router.get("/secured", middleware, async (err, req, res) => {
-    if (err){
+    if (typeof err !== 'undefined' && err){
         console.log(err.message);
         res.status(401).send("Access denied: "+err.message);
         return;
